f29b387f9a1ce5c5dfa1a29b8758ff9d87569465
Анастасия Лихашва <kassiopea308@mail.ru>
2024-01-19 20:49:46 +0300

    add: added the solution to the 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const pathStyles = path.join(__dirname, 'styles');
+const pathBundle = path.join(__dirname, 'project-dist', 'bundle.css');
+
+fs.readdir(pathStyles, {withFileTypes: true}, function(error, files) {
+    let result = [];
+    if (error) console.log(error);
+
+    fs.writeFile(pathBundle, '', function(error) {
+        if (error) console.log(error);
+    });
+
+    for (let i = 0; i < files.length; i++) {
+        if (files[i].isFile() && path.extname(files[i].name) === '.css') {
+            fs.readFile(path.join(pathStyles, files[i].name), 'utf-8', function (error, data) {
+                if (error) console.log(error);
+                result.push(data);
+                fs.appendFile(pathBundle, data, function(err) {
+                    if (err) console.log(err);
+                });
+              });
+        }
+    }
+
+})
+
